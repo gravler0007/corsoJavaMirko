4d1975baa50933e8664d07bc468056b9befa460f
Flavio <chiarappa_flavio.com>
2024-05-16 08:57:24 +0200

    esercizio scanner

--- codice java/15 mercoledi/EsercizioScanner.java
+++ codice java/15 mercoledi/EsercizioScanner.java
@@ -3,13 +3,64 @@ import java.util.Scanner;
 public class EsercizioScanner {
     public static void main(String args[]) {
 
-        Scanner scanner = new Scanner(System.in);
+        int rispostaCalcolo, punteggio = 0;
+        String username;
+
+        Scanner scannerN = new Scanner(System.in);
+        Scanner scannerS = new Scanner(System.in);
 
         System.out.print("Dammi il tuo username: ");
-        int input = scanner.nextInt();
+        username = scannerS.nextLine();
 
-        System.out.println("Ciao user, ti chiami " + input);
+        //PRIMA DOMANDA
+        System.out.print("\nQuanto fa 6 + 6?\nRisposta: ");
+        rispostaCalcolo = scannerN.nextInt();
 
-        scanner.close();
+        if (rispostaCalcolo == 12) {
+            punteggio += 5;
+        }
+
+        // SECONDA DOMANDA
+        System.out.print("\nQuanto fa 6 * 6?\nRisposta: ");
+        rispostaCalcolo = scannerN.nextInt();
+
+        if (rispostaCalcolo == 36) {
+            punteggio += 15;
+        } else {
+            punteggio -= 5;
+
+            if(punteggio < 0)
+                punteggio = 0;
+        }
+
+        // TERZA DOMANDA
+        System.out.print("\nQuanto fa 6 ^ 6?\nRisposta: ");
+        rispostaCalcolo = scannerN.nextInt();
+
+        if (rispostaCalcolo == 46656) {
+            punteggio += 30;
+        } else {
+            punteggio -= 5;
+
+            if (punteggio < 0)
+                punteggio = 0;
+        }
+
+        System.out.print("Utente '" + username + "' hai totalizzato " + punteggio + " punti! ");
+        
+        if (punteggio == 0)
+            System.out.print("Hai ampio margine di miglioramento!");
+        else if (punteggio >= 5 && punteggio <= 10)
+            System.out.print("Si può sempre migliorare!");
+        else if (punteggio >10 && punteggio <= 30)
+            System.out.print("NOT BAD!");
+        else if (punteggio > 30 && punteggio <= 45)
+            System.out.print("YOU'R A MONSTER!");
+        else {
+            System.out.print("\"YOU'R PURFFECT!\"");
+        }
+
+        scannerN.close();
+        scannerS.close();
     }
 }
