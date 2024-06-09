1244c184d1fd60d20ac09d2b89af36c92b7b7947
Flavio Chiarappa <chiarappa_flavio@yahoo.it>
2024-06-09 14:47:10 +0200

    elimina versione2 funzionante

--- codiceSpring/Giugno/8_sabato/Ex_1/demo/src/main/java/com/example/demo/RicettaController.java
+++ codiceSpring/Giugno/8_sabato/Ex_1/demo/src/main/java/com/example/demo/RicettaController.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,6 +50,28 @@ public class RicettaController {
     public String eliminaRicetta(@RequestParam Long id) {
         ricettaService.deleteById(id);
         return "redirect:/";
+    }
+
+    @GetMapping("/elimina2")
+    public String mostraFormEliminaRicette2() {
+        return "Elimina2";
+    }
+
+
+    // Con questa configurazione, quando un utente invia una richiesta DELETE a
+    // /ricette/{id}, il controller Spring chiamerà il metodo
+    // eliminaRicettaByApiFetch e
+    // passerà l'ID della ricetta da eliminare. Il servizio RicettaService quindi
+    // eliminerà la ricetta corrispondente dal database.
+    @DeleteMapping("/elimina2/{id}")
+    public ResponseEntity<Void> eliminaRicettaByApiFetch(@PathVariable Long id) {
+        Optional<Ricetta> ricetta = ricettaService.findById(id);// @PathVariable è un'annotazione in Spring MVC che viene utilizzata per mappare parte dell'URL di una richiesta HTTP a un parametro di un metodo di gestione della richiesta.
+        if (ricetta.isPresent()) {
+            ricettaService.deleteById(id);
+            return ResponseEntity.noContent().build(); // 204 No Content
+        } else {
+            return ResponseEntity.notFound().build(); // 404 Not Found
+        }
     }
     
 
